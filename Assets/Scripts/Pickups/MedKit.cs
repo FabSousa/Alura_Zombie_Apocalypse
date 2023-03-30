841d3caedb5fb9f9f02129d529abb74ea7f86816
Fabio <f.sousasp@hotmail.com>
2023-03-30 16:56:54 -0300

    Add medKit to zombies drop

--- Assets/Scripts/Pickups/MedKit.cs
+++ Assets/Scripts/Pickups/MedKit.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class MedKit : MonoBehaviour
 {
+    private float lifeTime = 5;
     private int minHeal = 10;
     private int maxHeal = 20;
     private int healValue;
 
     private void Start(){
         healValue = (int)Random.Range(minHeal, maxHeal);
+        Destroy(gameObject, lifeTime);
     }
 
     private void OnTriggerEnter(Collider other){
