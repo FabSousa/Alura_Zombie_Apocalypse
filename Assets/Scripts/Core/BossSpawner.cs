8b7bb820a1d70025246ccf2e7aa029fbe402b960
Fabio <f.sousasp@hotmail.com>
2023-04-28 17:06:06 -0300

    Increase spawnpoints for boss

--- Assets/Scripts/Core/BossSpawner.cs
+++ Assets/Scripts/Core/BossSpawner.cs
@@ -6,27 +6,44 @@ public class BossSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject bossPref;
     [SerializeField] private float timeToSpawn = 60;
-    private float timeToNextSpawn;
+    [SerializeField] private Transform[] spawnPoint;
     private UiController uiController;
+    private Transform playerPos;
+    private float timeToNextSpawn;
     private float warningTextDuration = 1;
     private float warningTextFadeDuration = 3;
 
-    void Awake(){
+    private void Awake(){
         uiController = GameObject.FindObjectOfType<UiController>() as UiController;
+        playerPos = GameObject.FindWithTag(Strings.PlayerTag).transform;
     }
 
-    void Start()
+    private void Start()
     {
         timeToNextSpawn = timeToSpawn;
     }
 
-    void Update()
+    private void Update()
     {
         if(Time.timeSinceLevelLoad >= timeToNextSpawn){
             uiController.ShowBossSpawnWarning(warningTextDuration, warningTextFadeDuration);
-            Instantiate(bossPref, transform.position, Quaternion.identity);
+            Instantiate(bossPref, FindSpawnPoint(), Quaternion.identity);
             timeToNextSpawn = timeToSpawn + Time.timeSinceLevelLoad;
         }
+    }
+
+    private Vector3 FindSpawnPoint(){
+        Vector3 spawn = Vector3.zero;
+        float greaterDistance = 0;
+        foreach (Transform pos in spawnPoint)
+        {
+            float distance = Vector3.Distance(playerPos.position, pos.position);
+            if(distance > greaterDistance){
+                greaterDistance = distance;
+                spawn = pos.position;
+            }
+        }
+        return spawn;
     }
 
     private void OnDrawGizmos()
